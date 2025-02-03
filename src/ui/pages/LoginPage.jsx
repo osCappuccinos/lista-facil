924abbce6f0537d13999ba485ded72d3eaeeaea1
Lucas Xavier <lucaseoxavier@gmail.com>
2025-02-03 20:19:22 -0300

    feat: login page css

--- src/ui/pages/LoginPage.jsx
+++ src/ui/pages/LoginPage.jsx
@@ -25,30 +25,28 @@ const LoginPage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white px-6">
-      {/* Botão de voltar */}
-      <div className="absolute top-8 left-6 cursor-pointer" onClick={() => navigate(-1)}>
-        <ArrowLeft size={28} className="text-red-700" />
+      <div className="absolute top-14 left-151 cursor-pointer" onClick={() => navigate(-1)}>
+        <ArrowLeft size={24} className="text-red-700" />
       </div>
 
-      <h1 className="text-2xl font-semibold text-gray-700 mb-6">Entre agora</h1>
+      <h1 className="text-[24px] font-semibold text-[#656565] mb-6">Entre agora</h1>
 
-      {/* Formulário */}
       <form className="w-full max-w-sm" onSubmit={handleLogin}>
-        <label className="block text-gray-700 text-sm font-medium mb-1">Senha</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full border border-gray-300 rounded-md p-3 mb-4 focus:outline-none focus:ring-2 focus:ring-red-700"
-          required
-        />
-
-        <label className="block text-gray-700 text-sm font-medium mb-1">Email</label>
+      <label className="block text-[#455A64] text-[14px] text-left font-medium mb-1">Email</label>
         <input
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="w-full border border-gray-300 rounded-md p-3 mb-6 focus:outline-none focus:ring-2 focus:ring-red-700"
+          class="w-full border border-[#CFD8DC]  rounded-md p-3 mb-6 focus:outline-none focus:ring-2 focus:ring-[#BF360C] text-black"
+          required
+        />
+
+        <label className="block text-[#455A64] text-[14px] text-left font-medium mb-1">Senha</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full border border-gray-300 rounded-md p-3 mb-4 focus:outline-none focus:ring-2 focus:ring-[#BF360C] text-black"
           required
         />
 
@@ -56,15 +54,15 @@ const LoginPage = () => {
 
         <button
           type="submit"
-          className="w-full bg-red-700 text-white py-3 rounded-lg shadow-md text-lg"
+          class="w-full !bg-[#BF360C] text-white mt-4 rounded-lg shadow-md text-[16px]"
         >
           ENTRAR
         </button>
       </form>
 
-      <p className="mt-6 text-gray-600 text-sm">
+      <p class="mt-6 text-[#888888] text-[12px]">
         NÃO POSSUI UMA CONTA?{" "}
-        <Link to="/register" className="text-red-700 font-bold">
+        <Link to="/register" class="!text-[#BF360C] font-bold">
           CRIE UMA AQUI
         </Link>
       </p>
